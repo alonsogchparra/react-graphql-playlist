6936778032db81492b8e453bdf9e2ca851585560
Alonso Parra <alonsogparra@gmail.com>
2020-03-06 22:23:00 -0400

    app fixed

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const grapqlHTTP = require('express-graphql');
+const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 
 const db = 'here you add the mongodb you created locally or in mongodb Atlas';
@@ -9,15 +9,15 @@ const db = 'here you add the mongodb you created locally or in mongodb Atlas';
 mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.once('open' , () => {
   console.log('connected to database');
-})
+});
 
 const app = express();
 
 app.use(cors());
 
-app.use('/graphql', grapqlHTTP({
+app.use('/graphql', graphqlHTTP({
   schema,
-  graphiql:true
+  graphiql: true
 }));
 
 app.listen(4000, () => {
