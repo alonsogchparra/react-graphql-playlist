f2ce58e6cf63218b6f45ae7659ed4f7ddac5f331
Alonso Parra <alonsogparra@gmail.com>
2020-03-11 21:08:11 -0400

    Adding apollo elements to app file

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
 import BookList from './components/BookList';
 
+const client = new ApolloClient ({
+  uri: 'http://localhost/4000/graphql'
+});
+
 function App() {
   return (
-    <div className="main">
-      <h1>Ninja Reading List</h1>
-      <BookList />
-    </div>
+    <ApolloProvider client={client}>
+      <div className="main">
+        <h1>Ninja Reading List</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
 
