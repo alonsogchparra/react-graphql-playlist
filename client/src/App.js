dbecc5d90db926a4a45e402d9e0128241c435076
Alonso Parra <alonsogparra@gmail.com>
2020-03-11 21:32:04 -0400

    getBooksQuery fixed

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import { ApolloProvider } from 'react-apollo';
 import BookList from './components/BookList';
 
 const client = new ApolloClient ({
-  uri: 'http://localhost/4000/graphql'
+  uri: 'http://localhost:4000/graphql'
 });
 
 function App() {
