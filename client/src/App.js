17b8d575c4cdc13c51900b8ea75fad86304dfd1a
Alonso Parra <alonsogparra@gmail.com>
2020-03-13 22:10:33 -0400

    className changed for id

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ const client = new ApolloClient ({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="main">
+      <div id="main">
         <h1>Ninja Reading List</h1>
         <BookList />
         <AddBook />
